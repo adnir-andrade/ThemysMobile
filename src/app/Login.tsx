1ec12b868db42266e9c08cda70b465fa7435a890
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-27 22:46:10 -0300

    Implement login screen

---
+++ src/app/Login.tsx
@@ -0,0 +1,34 @@
+import { View } from "react-native";
+import React, { useState } from "react";
+import LoginButton from "../components/LoginButton";
+import Background from "../components/ui/Background";
+import Logo from "../components/ui/Logo";
+import LoginForm from "../components/LoginForm";
+import { login } from "../services/authService";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      await login(email, password);
+      //   router.push("/select-character");
+    } catch (error: any) {}
+  };
+
+  return (
+    <Background>
+      <View className="flex-1 justify-center">
+        <Logo />
+        <LoginForm
+          email={email}
+          password={password}
+          setEmail={setEmail}
+          setPassword={setPassword}
+        />
+        <LoginButton onPress={handleLogin} />
+      </View>
+    </Background>
+  );
+}
