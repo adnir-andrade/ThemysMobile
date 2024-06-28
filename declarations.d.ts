21ed511180b33f9b549512e1c9b8bda9d29b29a6
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-27 22:20:14 -0300

    Add more declarations to className

--- declarations.d.ts
+++ declarations.d.ts
@@ -7,4 +7,10 @@ declare module "react-native" {
   interface ViewProps {
     className?: string;
   }
+  interface ImageBackgroundProps {
+    className?: string;
+  }
+  interface TouchableOpacityProps {
+    className?: string;
+  }
 }
