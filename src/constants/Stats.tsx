0418b45435def4c96e50a771b248aced63568d3c
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-29 19:06:09 -0300

    Implement Stats constant

---
+++ src/constants/Stats.tsx
@@ -0,0 +1,18 @@
+export function getStatsName() {
+  const stats = [
+    "Strenght",
+    "Dexterity",
+    "Constitution",
+    "Intelligence",
+    "Wisdom",
+    "Charisma",
+  ];
+
+  return stats;
+}
+
+export function getStatsAcronym(): string[] {
+  const stats = ["STR", "DEX", "CON", "INT", "WIS", "CHA"];
+
+  return stats;
+}
