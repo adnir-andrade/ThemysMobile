5adcbdc7fc691688f3ae5e935356ddab8e08f0be
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-29 18:12:26 -0300

    Implement Step1 and Step2

--- src/app/CreateCharacter/Step1.tsx
+++ src/app/CreateCharacter/Step1.tsx
@@ -1,10 +1,45 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { levels } from "../../constants/Levels";
+import { genders } from "../../constants/Genders";
+import { races } from "../../constants/Races";
+
+import AttributeDropdown from "./AttributeDropdown";
 
 export default function Step1() {
+  const [chosenLevel, setChosenLevel] = useState(null);
+  const [chosenGender, setChosenGender] = useState(null);
+  const [chosenRace, setChosenRace] = useState(null);
+
   return (
-    <View>
-      <Text>Step1</Text>
-    </View>
+    <>
+      <AttributeDropdown
+        itemsList={levels}
+        value={chosenLevel}
+        setValue={setChosenLevel}
+        title="Character Level"
+        placeholder="Level"
+        zIndex={3000}
+        zIndexInverse={1000}
+      />
+      <AttributeDropdown
+        itemsList={genders}
+        value={chosenGender}
+        setValue={setChosenGender}
+        title="Gender"
+        placeholder="Gender"
+        zIndex={2000}
+        zIndexInverse={2000}
+      />
+      <AttributeDropdown
+        itemsList={races}
+        value={chosenRace}
+        setValue={setChosenRace}
+        title="Race"
+        placeholder="Race"
+        zIndex={1000}
+        zIndexInverse={3000}
+        dropDownDirection="TOP"
+      />
+    </>
   );
 }
--- src/app/CreateCharacter/Step2.tsx
+++ src/app/CreateCharacter/Step2.tsx
@@ -1,10 +1,34 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { levels } from "../../constants/Levels";
+import { klasses } from "../../constants/Klasses";
+
+import AttributeDropdown from "./AttributeDropdown";
 
 export default function Step2() {
+  const [chosenKlass, setChosenKlass] = useState(null);
+  const [chosenKlassLevel, setChosenKlassLevel] = useState(null);
+
   return (
-    <View>
-      <Text>Step2</Text>
-    </View>
+    <>
+      <AttributeDropdown
+        itemsList={klasses}
+        value={chosenKlass}
+        setValue={setChosenKlass}
+        title="Class"
+        placeholder="Class"
+        zIndex={2000}
+        zIndexInverse={1000}
+      />
+      <AttributeDropdown
+        itemsList={levels}
+        value={chosenKlassLevel}
+        setValue={setChosenKlassLevel}
+        title="Class Level"
+        placeholder="Level"
+        zIndex={1000}
+        zIndexInverse={2000}
+        dropDownDirection="TOP"
+      />
+    </>
   );
 }
