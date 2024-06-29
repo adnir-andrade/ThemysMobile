98402fe5dd150671a09824839da2bf200d9550c4
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-29 19:42:27 -0300

    WIP: Implement Step3 layout

--- src/app/CreateCharacter/Step3.tsx
+++ src/app/CreateCharacter/Step3.tsx
@@ -1,10 +1,39 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { getStatsAcronym } from "../../constants/Stats";
+import StatLine from "./StatLine";
+import DiceButton from "../../components/DiceButton";
+import ResetButton from "../../components/ResetButton";
 
 export default function Step3() {
+  const stats = getStatsAcronym();
+
+  const handleDice = () => {
+    console.log("I am so tired");
+  };
+
+  const handleReset = () => {
+    console.log("I am so tired");
+  };
+
   return (
     <View>
-      <Text>Step3</Text>
+      <View className="flex flex-row mb-6">
+        <View className="flex-1">
+          <DiceButton className="self-center" onPress={handleDice} />
+        </View>
+        <View className="flex-1">
+          <Text className="text-center">0</Text>
+        </View>
+        <View className="flex-1">
+          <ResetButton className="self-center" onPress={handleReset} />
+        </View>
+      </View>
+      <ScrollView className="p-4">
+        {stats.map((stat, index) => (
+          <StatLine key={index} stat={stat} />
+        ))}
+      </ScrollView>
     </View>
   );
 }
