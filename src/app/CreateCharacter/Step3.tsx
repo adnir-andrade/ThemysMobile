500cd295524c1295f49422e16d9983d90c77ef00
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-29 20:21:02 -0300

    WIP: Implement Step3 screen

--- src/app/CreateCharacter/Step3.tsx
+++ src/app/CreateCharacter/Step3.tsx
@@ -2,8 +2,7 @@ import { View, Text, ScrollView } from "react-native";
 import React, { useState } from "react";
 import { getStatsAcronym } from "../../constants/Stats";
 import StatLine from "./StatLine";
-import DiceButton from "../../components/DiceButton";
-import ResetButton from "../../components/ResetButton";
+import ImageButton from "../../components/ImageButton";
 
 export default function Step3() {
   const stats = getStatsAcronym();
@@ -16,17 +15,33 @@ export default function Step3() {
     console.log("I am so tired");
   };
 
+  const handleDecrease = () => {
+    console.log("Should decrease");
+  };
+
+  const handleIncrease = () => {
+    console.log("Should increase");
+  };
+
   return (
     <View>
       <View className="flex flex-row mb-6">
         <View className="flex-1">
-          <DiceButton className="self-center" onPress={handleDice} />
+          <ImageButton
+            imageName="dice"
+            className="self-center"
+            onPress={handleDice}
+          />
         </View>
         <View className="flex-1">
           <Text className="text-center">0</Text>
         </View>
         <View className="flex-1">
-          <ResetButton className="self-center" onPress={handleReset} />
+          <ImageButton
+            imageName="reset"
+            className="self-center"
+            onPress={handleReset}
+          />
         </View>
       </View>
       <ScrollView className="p-4">
@@ -34,6 +49,25 @@ export default function Step3() {
           <StatLine key={index} stat={stat} />
         ))}
       </ScrollView>
+      <View className="flex flex-row mb-6">
+        <View className="flex-1">
+          <ImageButton
+            imageName="arrowDown"
+            className="self-center"
+            onPress={handleDecrease}
+          />
+        </View>
+        <View className="flex-1">
+          <Text className="text-center">0</Text>
+        </View>
+        <View className="flex-1">
+          <ImageButton
+            imageName="arrowUp"
+            className="self-center"
+            onPress={handleIncrease}
+          />
+        </View>
+      </View>
     </View>
   );
 }
