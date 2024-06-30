0fa1ae09f3f0126f9b704317970a8fb8e1e8dbb7
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-30 13:29:57 -0300

    Fully implement Edit Page

--- src/app/EditCharacter/EditCharacter.tsx
+++ src/app/EditCharacter/EditCharacter.tsx
@@ -1,39 +1,20 @@
-import { View } from "react-native";
 import React from "react";
 import Background from "../../components/ui/Background";
 import Logo from "../../components/ui/Logo";
-import Header from "../../components/Header";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../types/Navigation";
 import EditForm from "./EditForm";
-import ImageButton from "../../components/ImageButton";
 
-type Props = NativeStackScreenProps<RootStackParamList, "ViewCharacter">;
+type Props = NativeStackScreenProps<RootStackParamList, "EditCharacter">;
 
 export default function EditCharacter({ navigation, route }: Props) {
   const { character } = route.params;
 
-  console.log(character);
-  const handleConfig = () => {
-    console.log("Click Click I am goddamn hungry!");
-  };
-
   return (
     <Background>
-      <View className="flex-1">
-        <Logo />
-        <Header
-          title={`${character.name}`}
-          color="text-orange-300"
-          className="mt-20"
-        />
-        <EditForm character={character} />
-        <ImageButton
-          imageName="config"
-          className="mt-20 place-self-end self-center"
-          onPress={handleConfig}
-        />
-      </View>
+      <Logo />
+
+      <EditForm navigation={navigation} route={route} character={character} />
     </Background>
   );
 }
--- src/app/EditCharacter/EditForm.tsx
+++ src/app/EditCharacter/EditForm.tsx
@@ -1,35 +1,83 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TextInput } from "react-native";
+import React, { useState } from "react";
 import { Character } from "../../types/Character";
+import HeaderInput from "../../components/HeaderInput";
+import AttributeDropdown from "../CreateCharacter/AttributeDropdown";
+import { genders } from "../../constants/Genders";
+import ImageButton from "../../components/ImageButton";
+import { editCharacter } from "../../services/characterService";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../../types/Navigation";
 
 type Props = {
   character: Character;
-};
+} & NativeStackScreenProps<RootStackParamList, "EditCharacter">;
 
-export default function EditForm({ character }: Props) {
+export default function EditForm({ character, navigation }: Props) {
+  const [newName, setNewName] = useState<string>(character.name);
+  const [newGender, setNewGender] = useState<string | null>(
+    character.gender ?? ""
+  );
+  const [newBackground, setNewBackground] = useState<string | null>(
+    character.background ?? ""
+  );
+
+  const handleCancel = () => {};
+
+  const handleAccept = async () => {
+    await editCharacter(
+      {
+        name: newName,
+        gender: newGender!,
+        background: newBackground!,
+      },
+      character.id!
+    );
+
+    navigation.reset({
+      index: 0,
+      routes: [{ name: "SelectCharacter" }],
+    });
+  };
+
   return (
-    <View className="mt-16">
-      <View className="justify-start items-center mb-12">
-        <Text className="text-3xl text-white font-semibold">
-          {character.gender} {character.race}
-        </Text>
-        <Text>
-          <Text className="text-2xl text-white">Level {character.level} </Text>
-          <Text className="text-2xl text-white font-semibold">
-            {character.klass}
-          </Text>
-        </Text>
-        <Text className="text-lg text-white font-semibold">
-          HP: {character.hp}
-        </Text>
+    <>
+      <HeaderInput
+        value={newName}
+        onChangeText={setNewName}
+        color="text-orange-300"
+        className="pt-20"
+        mb="4"
+      />
+      <View className="mb-2 px-8">
+        <AttributeDropdown
+          itemsList={genders}
+          value={newGender}
+          setValue={setNewGender}
+          title="Gender"
+          placeholder="Gender"
+          zIndex={1000}
+          zIndexInverse={2000}
+          dropDownDirection="TOP"
+        />
       </View>
 
-      <View className="px-7 mx-4 bg-black/50 py-8 rounded-xl">
+      <View className="px-7 mx-4 bg-black/50 py-5 rounded-xl h-3/6">
         <Text className="text-2xl text-white font-semibold">Background</Text>
-        <Text className="text-xl text-white font-medium">
-          {character.background}
-        </Text>
+        <TextInput
+          className="text-xl text-white font-medium text-pretty pb-1"
+          cursorColor="#CFBE86"
+          multiline={true}
+          value={newBackground!}
+          onChangeText={setNewBackground}
+          placeholder="This is the epic story of..."
+          placeholderTextColor="white"
+        ></TextInput>
       </View>
-    </View>
+      <View className="flex flex-row justify-between items-center absolute bottom-0 left-0 right-0 pb-32 px-20">
+        <ImageButton imageName="cancel" onPress={handleCancel} />
+        <ImageButton imageName="accept" onPress={handleAccept} />
+      </View>
+    </>
   );
 }
