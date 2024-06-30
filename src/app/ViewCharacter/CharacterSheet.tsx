d2a6a4153fb4ca560ef08ac1ec7bdc0e67aca7dd
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-30 13:39:17 -0300

    Refactor View into ScrollView to read Bio

--- src/app/ViewCharacter/CharacterSheet.tsx
+++ src/app/ViewCharacter/CharacterSheet.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { Character } from "../../types/Character";
 import Stat from "./Stat";
@@ -24,7 +24,7 @@ export default function CharacterSheet({ character }: Props) {
           HP: {character.hp}
         </Text>
       </View>
-      <View className="flex flex-row justify-between items-center mb-12">
+      <View className="flex flex-row justify-between items-center mb-6">
         <Stat stat="STR" value={character.strength} />
         <Stat stat="DEX" value={character.dexterity} />
         <Stat stat="CON" value={character.constitution} />
@@ -33,11 +33,16 @@ export default function CharacterSheet({ character }: Props) {
         <Stat stat="CHA" value={character.charisma} />
       </View>
 
-      <View className="px-7 mx-4 bg-black/50 py-8 rounded-xl">
-        <Text className="text-2xl text-white font-semibold">Background</Text>
-        <Text className="text-xl text-white font-medium">
-          {character.background}
-        </Text>
+      <View className="px-7 mx-4 bg-black/50 py-8 rounded-xl h-3/6">
+        <ScrollView className="mx-5">
+          <Text className="text-2xl text-white font-semibold mb-2">
+            Background
+          </Text>
+
+          <Text className="text-xl text-white font-medium leading-7">
+            {character.background}
+          </Text>
+        </ScrollView>
       </View>
     </View>
   );
