f1c11b03c9b8fbb380c48ec87cc8a2741a9502d1
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-30 14:17:28 -0300

    Implement Avatar component

--- src/app/SelectCharacter/SelectCharacter.tsx
+++ src/app/SelectCharacter/SelectCharacter.tsx
@@ -6,12 +6,14 @@ import Header from "../../components/Header";
 import CharacterList from "./CharacterList";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../types/Navigation";
+import Avatar from "../../components/ui/Avatar";
 
 type Props = NativeStackScreenProps<RootStackParamList, "SelectCharacter">;
 
 export default function SelectCharacter({ navigation, route }: Props) {
   return (
     <Background>
+      <Avatar />
       <View className="justify-center">
         {/* <Logo /> */}
         <Header title="Characters" color="text-epic" className="mt-20 mb-10" />
---
+++ src/components/ui/Avatar.tsx
@@ -0,0 +1,25 @@
+import { Pressable, Image, PressableProps } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../../types/Navigation";
+
+type Props = {} & PressableProps;
+
+export default function Avatar({ ...rest }: Props) {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+  const handleAvatar = () => {
+    // navigation.navigate("Profile");
+  };
+
+  return (
+    <Pressable onPress={handleAvatar} {...rest}>
+      <Image
+        source={require("../../../assets/images/profile/profile1.jpg")}
+        resizeMode="cover"
+        className="rounded-full h-14 w-14 self-end mx-4 mt-4"
+      ></Image>
+    </Pressable>
+  );
+}
