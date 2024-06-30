44847836371e842cf133a1fcef6659558c62a588
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-30 04:25:17 -0300

    Removed unused references

--- src/app/CreateCharacter/StatLine.tsx
+++ src/app/CreateCharacter/StatLine.tsx
@@ -5,8 +5,6 @@ import {
   TouchableOpacityProps,
 } from "react-native";
 import React, { useEffect, useState } from "react";
-import { useCharacter } from "../../hooks/useCharacter";
-import { acronymToAttribute } from "../../constants/Stats";
 
 type Props = {
   stat: string;
