d3dda6044fbb51c50b2664adad06476bf8572154
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-30 05:15:37 -0300

    Implement Skills constant

---
+++ src/constants/Skills.tsx
@@ -0,0 +1,26 @@
+function getSkills() {
+  const skills: string[] = [
+    "Athletics",
+    "Acrobatics",
+    "Sleight of Hand",
+    "Stealth",
+    "Arcana",
+    "History",
+    "Investigation",
+    "Nature",
+    "Religion",
+    "Animal Handling",
+    "Insight",
+    "Medicine",
+    "Perception",
+    "Survival",
+    "Deception",
+    "Intimidation",
+    "Performance",
+    "Persuasion",
+  ];
+
+  return skills;
+}
+
+export const skills = getSkills();
