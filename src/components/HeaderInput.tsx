02a4f4e76ef327db423fde008a7a56400b935cc0
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-30 13:29:46 -0300

    Refactor Header one more time

--- src/components/HeaderInput.tsx
+++ src/components/HeaderInput.tsx
@@ -4,12 +4,18 @@ import React from "react";
 type Props = {
   title?: string;
   color?: string;
+  mb?: string;
 } & TextInputProps;
 
-export default function HeaderInput({ title, color, ...rest }: Props) {
+export default function HeaderInput({
+  title,
+  color,
+  mb = "20",
+  ...rest
+}: Props) {
   const textStyle = "text-4xl font-semibold text-center capitalize " + color;
   return (
-    <View className="mb-20">
+    <View className={`mb-${mb}`}>
       <TextInput
         className={textStyle}
         placeholder="Insert name here"
