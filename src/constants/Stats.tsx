6a551af740c7a7b248acac7323b4a8c0142728eb
Adnir Andrade <adnir_andrade@hotmail.com>
2024-06-30 02:47:45 -0300

    Implement AcronymToAttribute function

--- src/constants/Stats.tsx
+++ src/constants/Stats.tsx
@@ -1,3 +1,5 @@
+import { Character } from "../types/Character";
+
 export function getStatsName() {
   const stats = [
     "Strenght",
@@ -15,4 +17,24 @@ export function getStatsAcronym(): string[] {
   const stats = ["STR", "DEX", "CON", "INT", "WIS", "CHA"];
 
   return stats;
+}
+
+export function acronymToAttribute(
+  acronym: string,
+  character: Character
+): number | undefined {
+  type RelationType = {
+    [key: string]: number;
+  };
+
+  const relation: RelationType = {
+    STR: character.strength,
+    DEX: character.dexterity,
+    CON: character.constitution,
+    INT: character.intelligence,
+    WIS: character.wisdom,
+    CHA: character.charisma,
+  };
+
+  return relation[acronym] || 10;
 }
